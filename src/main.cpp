d146a931a9b94c17a9032a61286395113d19ad30
viastolfi <vincent.astolfi12@gmail.com>
2026-03-26 10:43:44 +0100

    feat: add raygui thirparty

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
+#include "raylib.h"
 
-int main(int argc, char** argv) 
+#define RAYGUI_IMPLEMENTATION
+#include "thirdparty/raygui.h"
+
+int main()
 {
-  std::cout << "Hello World!" << std::endl;
+  InitWindow(400, 200, "raygui - controls test suite");
+  SetTargetFPS(60);
+
+  bool showMessageBox = false;
+
+  while (!WindowShouldClose())
+  {
+    BeginDrawing();
+    ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+
+    if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+
+    if (showMessageBox)
+    {
+      int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
+          "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+
+      if (result >= 0) showMessageBox = false;
+    }
+
+    EndDrawing();
+  }
+
+  CloseWindow();
   return 0;
 }
